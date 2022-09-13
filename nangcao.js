3c107df2a72c984998cf57698d7556f96c186495
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-09-13 14:38:34 +0700

    first commit

---
+++ nangcao.js
@@ -0,0 +1,15 @@
+let input=document.querySelector('.input');
+let items=document.querySelectorAll('.item');
+items.forEach(function(btn){
+    btn.addEventListener('click',function(){
+        if(input.innerHTML == '0')
+            input.innerHTML='';
+        if(btn.innerHTML=='AC')
+            input.innerHTML='0';
+        else if(btn.innerHTML=='='){
+            input.innerHTML=eval(input.innerHTML);
+        }
+        else input.innerHTML+=btn.innerHTML;
+    })
+})
+
